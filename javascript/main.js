9348b7e3b50079e0e15ca617b540e7bad3c8b05b
SYMist <mmist0226@gmail.com>
2022-02-01 21:40:32 +0900

    header

--- javascript/main.js
+++ javascript/main.js
@@ -14,3 +14,21 @@ searchInputEl.addEventListener('blur', function () {
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
 });
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function () {
+  console.log(window.scrollY);
+  if (window.scrollY > 500) {
+    // 배지 숨기기
+    gsap.to(badgeEl, .3, {
+      opacity: 0,
+      display: 'none'
+    });
+  } else {
+    gsap.to(badgeEl, .3, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+}, 300));
